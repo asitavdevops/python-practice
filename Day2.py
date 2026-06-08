cfd3c3c10a74824c256d63dae6fd7c3fe76839df
Asitav attanaik <git.asitav@gmail.com>
2026-06-08 23:43:03 +0530

    Day2 V2

--- Day2.py
+++ Day2.py
@@ -5,3 +5,21 @@ b=13.50
 c=123,345,23
 print(type(a),type(b),type(c))
 
+#Mathematical Operations (PEMDAS - LeftoRight)
+print(3 * 3 + 3 / 3 -3)
+print(3 * (3 + 3) / 3 -3)
+
+#Assignment Operator [+= -= *= /= ]
+score = 0
+score = score + 1
+print(score)
+score +=1
+print(score)
+score -=1
+print(score)
+
+# f-strings
+score = 0
+height = 5.8
+is_winning = True
+print(f"your score is :{score},Your Height is :{height},Are you winning:{is_winning}")
