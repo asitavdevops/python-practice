c3161f391b1231bccbd702677df2e8b534e3bac5
Asitav attanaik <git.asitav@gmail.com>
2026-06-08 18:28:15 +0530

    day1

---
+++ Day1.py
@@ -0,0 +1,7 @@
+#Printing :
+
+print('Hello world !') #python '' or "" is also used for defining strings
+
+#Strings Manipulation :
+print("Hello world\nHello World\n Hello World") #\n is used for new line)
+print("Hello" + " Asitav" + "\nHello" + "  Amitav") # + is used for concatenation of strings
