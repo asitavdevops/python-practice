f0586281a9b466544a78ec80b710d2e4f2fcf743
Asitav attanaik <git.asitav@gmail.com>
2026-06-09 00:04:34 +0530

    Day3

---
+++ Day3.py
@@ -0,0 +1,17 @@
+#If else condition && Operatore [> < >= <= == !=]
+"""
+if condition:
+    do this
+else:
+    do this
+"""
+
+print("Welcome to Roller costar ride !!")
+hg = int(input("Enter your hight in CM :"))
+
+if hg >= 120:
+    print("Congrats you can take the ride!!")
+else:
+    print("You cant enter the ride !! Sorry")   
+
+
