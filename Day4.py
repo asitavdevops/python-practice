b5ad928c9f946b47af0ad57887def023e8c93199
Asitav attanaik <git.asitav@gmail.com>
2026-06-10 23:42:02 +0530

    Day3 v4

---
+++ Day4.py
@@ -0,0 +1,6 @@
+# Randomization & python lists
+# Create random number between 0 and 20 using ramdom module (https://docs.python.org/3/library/random.html)
+import random
+random_integer = random.randint(0, 20)
+print(random_integer)
+
