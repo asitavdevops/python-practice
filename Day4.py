dae1acec597a46abfd033220085b65993f9abe5f
Asitav attanaik <git.asitav@gmail.com>
2026-06-12 17:19:50 +0530

    Day4 v2

--- Day4.py
+++ Day4.py
@@ -1,82 +1,82 @@
 # Randomization & python lists
 # Create random number between 0 and 20 using ramdom module (https://docs.python.org/3/library/random.html)
-# import random
-# random_integer = random.randint(0, 20)
-# print(random_integer)
+import random
+random_integer = random.randint(0, 20)
+print(random_integer)
 
-#random.random() - Return the next random floating-point number in the range 0.0 <= X < 1.0
-#random.uniform(a, b)-Return a random floating-point number N such that a <= N <= b 
-# random_number_0_to_1 = random.random() * 10
-# print(random_number_0_to_1)
+random.random() - Return the next random floating-point number in the range 0.0 <= X < 1.0
+random.uniform(a, b)-Return a random floating-point number N such that a <= N <= b 
+random_number_0_to_1 = random.random() * 10
+print(random_number_0_to_1)
 
-# random_float = random.uniform(1,10)
-# print(random_float)
+random_float = random.uniform(1,10)
+print(random_float)
 
 # Coin flip program for Heads and Tails 
-# random_heads_tails = random.randint(0, 1)
-# print(random_heads_tails)
+random_heads_tails = random.randint(0, 1)
+print(random_heads_tails)
 
-# if random_heads_tails == 0:
-#     print("Heads ")
-# else:
-#     print("Tails ")
+if random_heads_tails == 0:
+    print("Heads ")
+else:
+    print("Tails ")
 
 #List - ordered collection of items, can be of any data type, mutable (can be changed after creation), allows duplicate values
 #fruits = [item1, item2 ,item3]
 
 #display states of united states who joined first :
-# usa_states_by_admission = [
-#     "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
-#     "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
-#     "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky",
-#     "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi",
-#     "Illinois", "Alabama", "Maine", "Missouri", "Arkansas",
-#     "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
-#     "California", "Minnesota", "Oregon", "Kansas", "West Virginia",
-#     "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
-#     "Montana", "Washington", "Idaho", "Wyoming", "Utah",
-#     "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
-# ]
+usa_states_by_admission = [
+    "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+    "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
+    "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky",
+    "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi",
+    "Illinois", "Alabama", "Maine", "Missouri", "Arkansas",
+    "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+    "California", "Minnesota", "Oregon", "Kansas", "West Virginia",
+    "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
+    "Montana", "Washington", "Idaho", "Wyoming", "Utah",
+    "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+]
 
-# print(usa_states_by_admission[0])
-# print(usa_states_by_admission[-2])
+print(usa_states_by_admission[0])
+print(usa_states_by_admission[-2])
 
 #Change/subsitute item in list
-# usa_states_by_admission[-1] = "HAWAIL"
-# print(usa_states_by_admission[-1] )
+usa_states_by_admission[-1] = "HAWAIL"
+print(usa_states_by_admission[-1] )
 
 #adding item to the list end 
-# usa_states_by_admission.append("AsitavLand")
-# print(usa_states_by_admission)
+usa_states_by_admission.append("AsitavLand")
+print(usa_states_by_admission)
 
 #adding multiple items to the list end 
-# usa_states_by_admission.extend(["AmitavLand" , "SupriyaLand"])
-# print(usa_states_by_admission)
+usa_states_by_admission.extend(["AmitavLand" , "SupriyaLand"])
+print(usa_states_by_admission)
 
 #Code - Who will pay the Bill
 #solution 1:
-# import random
-# friends = ["Charli", "Bob", "Allice", "Katty", "Asitav", "Ravi"]
-# WPB = random.choice(friends)
-# print(f"Bill on :{WPB}")
-# #solution 2:
-# random_index = random.randint(0, 5)
-# print(f"2 solution who will pay the bill: {friends[random_index]}")
+import random
+friends = ["Charli", "Bob", "Allice", "Katty", "Asitav", "Ravi"]
+WPB = random.choice(friends)
+print(f"Bill on :{WPB}")
+#solution 2:
+random_index = random.randint(0, 5)
+print(f"2 solution who will pay the bill: {friends[random_index]}")
 
 #Index error(list index out of range) && Nested list:
-# test = ["Charli", "Bob", "Allice", "Katty", "Asitav", "Ravi"]
-# print(f"length of the test index is :{len(test)} : {test[5]}")
+test = ["Charli", "Bob", "Allice", "Katty", "Asitav", "Ravi"]
+print(f"length of the test index is :{len(test)} : {test[5]}")
 
 #Nested list example .#1. All food items whaich are using pestisides 
-#dirty_dozen = ["Strawberries", "Spinach", "Kale", "Grapes", "Peaches", "Pears", "Nectarines", "Apples", "Peppers", "Cherries", "Blueberries", "Green Beans"]
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Grapes", "Peaches", "Pears", "Nectarines", "Apples", "Peppers", "Cherries", "Blueberries", "Green Beans"]
 
 #segrigate vegitale and Fruits 
-# fruits = ["Strawberries", "Grapes", "Peaches", "Pears", "Nectarines", "Apples", "Cherries", "Blueberries"]
-# vegetables = ["Spinach", "Kale, Collard & Mustard Greens", "Bell & Hot Peppers", "Green Beans"]
+fruits = ["Strawberries", "Grapes", "Peaches", "Pears", "Nectarines", "Apples", "Cherries", "Blueberries"]
+vegetables = ["Spinach", "Kale, Collard & Mustard Greens", "Bell & Hot Peppers", "Green Beans"]
 
 # #now use nested list for above one example
-# dirty_dozen_nested_list = [ fruits, vegetables ]
-# print(dirty_dozen_nested_list)
+dirty_dozen_nested_list = [ fruits, vegetables ]
+print(dirty_dozen_nested_list)
 
 """
 fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
